05442f33aef2ea9fb22ded2be1a4354810165a11
DAVID-MATEU-SERRANO <100522337@alumnos.uc3m.es>
2025-10-13 23:49:36 +0200

    Tengo sueño, pero tranqui q esto es fácil

--- 2.1/gen-1.py
+++ 2.1/gen-1.py
@@ -1,1 +1,20 @@
-AAAAAAAAAAA
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) == 3:
+    input = sys.argv[1]   
+    output = sys.argv[2]  
+
+    with open(input, "r") as f:
+        try:
+            linea_temp = f.readline().split()
+            n, m = linea_temp[0], linea_temp[1]
+            linea_temp = f.readline().split()
+            kd, kp = linea_temp[0], linea_temp[1]
+            distancias = f.readline().split()
+            pasajeros = f.readline().split()
+
+        except:
+            print("Formato incorrecto del archivo de entrada")
+
+
